301bbfb236def63fc0bc374903b00f42010d4eba
Johan Mynhardt <johanmynhardt@gmail.com>
2016-02-18 17:48:29 +0200

    use wp-api-v2-client-java-meta-plugin

--- scripts/wp-cli-plugins-extra.sh
+++ scripts/wp-cli-plugins-extra.sh
@@ -6,4 +6,9 @@ echo "Running extra wp-cli tasks:"
 wp plugin install https://github.com/WP-API/Basic-Auth/archive/master.zip
 wp plugin activate Basic-Auth-master
 
+wp plugin install https://github.com/Afrozaar/wp-api-v2-client-java-meta-plugin/archive/master.zip
+wp plugin activate wp-api-v2-client-java-meta-plugin-master
+#wp plugin install https://github.com/Afrozaar/wp-api-v2-afrozaar-extras/archive/master.zip
+#wp plugin activate wp-api-v2-afrozaar-extras-master
+
 echo "Done running extra wp-cli tasks."
