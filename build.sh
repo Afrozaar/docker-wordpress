a99c75d565502fba28a1c4332044e8653cc529c0
Johan Mynhardt <johanmynhardt@gmail.com>
2016-03-30 13:18:27 +0200

    Add files to build a wordpress docker image against legacy WP 4.2.2
    Also add wp-json v1. (caxton troubleshoot)

--- build.sh
+++ build.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+# special flavour: 422
+if [ "" != "$FLAVOUR" ]; then
+  echo "Flavour: $FLAVOUR"
+fi
+
 # Build Docker images
-docker build -t afrozaar/ubuntu-mysql-apache-php -f ./base-docker/Dockerfile ./base-docker
-docker build -t afrozaar/wordpress .
+#docker build -t afrozaar/ubuntu-mysql-apache-php -f ./base-docker/Dockerfile$FLAVOUR ./base-docker
+
+if [ "" != "$FLAVOUR" ]; then
+  echo "Building with tag flavour: $FLAVOUR";
+  #docker build -t afrozaar/ubuntu-mysql-apache-php -f ./base-docker/Dockerfile$FLAVOUR ./base-docker
+  docker build -t afrozaar/wordpress:$FLAVOUR -f ./Dockerfile.$FLAVOUR .
+else
+  echo "Building normally"
+  #docker build -t afrozaar/ubuntu-mysql-apache-php -f ./base-docker/Dockerfile ./base-docker
+  docker build -t afrozaar/wordpress .
+fi;
+
+#docker build -t afrozaar/wordpress .
 
 # Add docker.dev to /etc/hosts
 ./add-docker.dev-to-hosts
---
+++ scripts/wp-plugins422.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+PLUGINS=(json-rest-api)
